54b85491f19ec8647ab076575886e42b11a287fa
cabbache <cabbache@cabbache.net>
2023-08-02 02:18:22 +0200

    serialization

--- trajectory/src/lib.rs
+++ trajectory/src/lib.rs
@@ -1,7 +1,10 @@
+use serde::Deserialize;
 use lazy_static::lazy_static;
 use wasm_bindgen::prelude::*;
 use std::f32::consts::{PI};
 use serde::{Serialize};
+use js_sys::Uint8Array;
+use bincode::{serialize, deserialize};
 
 const ACCELERATION: f32 = 200.0; //player acceleration
 const PROPEL_DIRECTION: f32 = -PI/2.0;
@@ -18,17 +21,24 @@ lazy_static! {
 	static ref DRAGSTEP: f32 = DRAG.powf(1f32 / TIMESTEP_FPS as f32);
 }
 
-pub const BODIES: [Body; 1] = [
+pub const BODIES: [Body; 2] = [
 	Body {
 		pos: Vector{
 			x: 0.0,
 			y: 0.0,
 		},
+		radius: 500.0,
+	},
+	Body {
+		pos: Vector{
+			x: 500.0,
+			y: 300.0,
+		},
 		radius: 300.0,
 	},
 ];
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 #[wasm_bindgen]
 pub struct Trajectory{
 	pub propelling: bool,
@@ -39,9 +49,9 @@ pub struct Trajectory{
 	pub time: u64,
 }
 
-//TODO make cos/sin faster by storing the results
 #[wasm_bindgen]
 impl Trajectory {
+	//euler's method
 	pub fn live(&self, time: u64) -> Trajectory{
 		let mut result = self.clone();
 		let spin_speed = (result.spin_direction as f32) * RADIANS_PER_SECOND;
@@ -86,9 +96,23 @@ impl Trajectory {
 		self.update(time);
 		self.propelling = on;
 	}
+
+	pub fn decode_trajectory(array: Uint8Array) -> Trajectory {
+		let vec: Vec<u8> = array.to_vec();
+		let trajectory: Trajectory = deserialize(&vec).unwrap();
+		trajectory
+	}
 }
 
-#[derive(Serialize, Debug, Clone, Copy)]
+//this is not exposed to wasm
+impl Trajectory {
+	pub fn encode(&self) -> Uint8Array {
+		let encoded = serialize(&self).unwrap();
+		Uint8Array::from(&encoded[..])
+	}
+}
+
+#[derive(Serialize, Debug, Clone, Copy, Deserialize)]
 #[wasm_bindgen]
 pub struct Vector{
 	pub x: f32,
